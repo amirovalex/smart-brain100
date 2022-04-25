2c4786650d06fb3a2e7d3d19a165b12e0ebbadaf
Sasha Amirov <amirovalex98@gmail.com>
2022-04-25 16:45:08 +0300

    added catch to signin function

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -36,7 +36,8 @@ class SignIn extends React.Component {
             this.props.onRouteChange("home");
           }
           this.setState({ isSigningIn: false });
-        });
+        })
+        .catch((err) => console.log(err));
     }
   };
 
