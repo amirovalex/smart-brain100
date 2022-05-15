9478e47ad870668bc099467c3413dcf0504f9fa3
Sasha Amirov <amirovalex98@gmail.com>
2022-05-15 13:31:23 +0300

    Updates

---
+++ precache-manifest.b95a64a965b2507910a6c0823767b149.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ed14c886b70307caa7a37b1d263e17",
+    "url": "/smart-brain100/index.html"
+  },
+  {
+    "revision": "b5f5f85ca15d1a06bb85",
+    "url": "/smart-brain100/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9b18fe0c02a9bbb874f7",
+    "url": "/smart-brain100/static/css/main.58de187d.chunk.css"
+  },
+  {
+    "revision": "b5f5f85ca15d1a06bb85",
+    "url": "/smart-brain100/static/js/2.2e82e884.chunk.js"
+  },
+  {
+    "revision": "9b18fe0c02a9bbb874f7",
+    "url": "/smart-brain100/static/js/main.3eed016e.chunk.js"
+  },
+  {
+    "revision": "8155d9c7e66e258caa41",
+    "url": "/smart-brain100/static/js/runtime~main.e3f09a91.js"
+  },
+  {
+    "revision": "3f726e51507ace62fe2806b11326b0e3",
+    "url": "/smart-brain100/static/media/brain.3f726e51.png"
+  }
+]);
